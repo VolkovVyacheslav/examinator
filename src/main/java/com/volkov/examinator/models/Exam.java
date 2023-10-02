26076cdf3060d4029de1463abaee46f0f5c6b1d8
VolkovVyacheslav <volchara.only@gmail.com>
2023-10-02 10:24:05 +0300

    init

---
+++ src/main/java/com/volkov/examinator/models/Exam.java
@@ -0,0 +1,19 @@
+package com.volkov.examinator.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Exam {
+
+    private String title;
+
+    private List<Section> sections;
+}
---
+++ src/main/java/com/volkov/examinator/models/Section.java
@@ -0,0 +1,18 @@
+package com.volkov.examinator.models;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Section {
+
+   private List<Question> questions;
+}
